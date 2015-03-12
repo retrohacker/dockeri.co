51ce492e3460a2eec24c4c5b9dcb228f6a63ef46
William Blankenship <william.jblankenship@gmail.com>
2015-03-11 21:27:07 -0500

    Uses new docker registry stars api

--- scrapper/index.js
+++ scrapper/index.js
@@ -5,10 +5,22 @@ var m = module.exports = function(namespace,repo, cb) {
   scrap("https://registry.hub.docker.com/"+prefix+namespace+"/"+repo,function(e,$) {
     if(e) return cb(e)
     var result = {}
-    result.stars = $('.stars').text().trim()
-    result.comments = $('.comments').text().trim()
-    result.downloads = $('.downloads').text().trim()
-    result.trusted = $('.trusted').length > 0
-    return cb(null,result)
+    try {
+      result.comments = $('.comments').first().text().trim()
+    } catch(e) {}
+    try {
+      result.downloads = $('.downloads').first().text().trim()
+    } catch(e) {}
+    try {
+      result.trusted = $('.trusted').length > 0
+    } catch(e) {}
+    if(namespace==='_') namespace = 'library' //allow us to use api
+    scrap('https://registry.hub.docker.com/v2/repositories/'+namespace+'/'+repo+'/stars/count/',function(e,$) {
+      if(e) return cb(e)
+      try {
+        result.stars = $.html()
+      } catch(e) {}
+      return cb(e,result)
+    })
   })
 }
