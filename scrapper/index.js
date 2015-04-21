ec604f48a1f7c402fadc4e6d32c71995edbdae4e
William Blankenship <william.jblankenship@gmail.com>
2015-04-21 11:55:56 -0500

    Handles stars api flaking out with grace.

--- scrapper/index.js
+++ scrapper/index.js
@@ -2,7 +2,8 @@ var scrap = require('scrap');
 
 var m = module.exports = function(namespace,repo, cb) {
   var prefix = namespace==='_'?'':'u/';
-  scrap("https://registry.hub.docker.com/"+prefix+namespace+"/"+repo,function(e,$) {
+  var url = "https://registry.hub.docker.com/"+prefix+namespace+"/"+repo
+  scrap(url,function(e,$) {
     if(e) return cb(e)
     var result = {}
     try {
@@ -15,12 +16,18 @@ var m = module.exports = function(namespace,repo, cb) {
       result.trusted = $('.trusted').length > 0
     } catch(e) {}
     if(namespace==='_') namespace = 'library' //allow us to use api
-    scrap('https://registry.hub.docker.com/v2/repositories/'+namespace+'/'+repo+'/stars/count/',function(e,$) {
-      if(e) return cb(e)
+    var starUrl = 'https://registry.hub.docker.com/v2/repositories/'+namespace+'/'+repo+'/stars/count/'
+    scrap(starUrl,function(e,$) {
+      if(e) {
+        // Stars endpoint apears to be volatile. Will return `?` if it fails on us.
+        // Better than not returning a badge.
+        result.stars = '?'
+        return cb(null,result)
+      }
       try {
         result.stars = $.html()
       } catch(e) {}
-      return cb(e,result)
+      return cb(null,result)
     })
   })
 }
