c239a6cfff120d5ab04b26c83d8e95780e4173e6
retrohacker <retrohacker@protonmail.com>
2019-01-30 19:38:51 -0800

    add page for generating badges

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,47 +1,10 @@
-// @architect/functions enables secure sessions, express-style middleware and more
-// let arc = require('@architect/functions')
-// let url = arc.http.helpers.url
+var fs = require('fs')
+var path = require('path')
+var html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8')
 
 exports.handler = async function http(req) {
-  console.log(req)
   return {
     type: 'text/html; charset=utf8',
-    body: '<h1>Hello world!</h1>'
+    body: html
   }
 }
-
-// Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http(request) {
-  if (process.env.NODE_ENV !== 'production') {
-    console.log(request)
-  }
-  return {
-    status: 302,
-    location: '/staging/about',
-  }
-}
-*/
-
-/* Successful resource creation, CORS enabled
-exports.handler = async function http(request) {
-  return {
-    status: 201,
-    type: 'application/json',
-    body: JSON.stringify({ok: true}),
-    cors: true,
-  }
-}
-*/
-
-/* Deliver client-side JS
-exports.handler = async function http(request) {
-  return {
-    type: 'text/javascript',
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
-
-// Learn more: https://arc.codes/guides/http
